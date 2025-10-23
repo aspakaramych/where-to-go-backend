3f8c5594ed22c0a45a1a5f9db6a3008c98d296d6
aspakaramych <vgrig2006@gmail.com>
2025-10-23 17:42:14 +0700

    Add places from db

--- whereToGo/places/urls.py
+++ whereToGo/places/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.show_map, name='map'),
+    path('places/', views.all_places_geojson, name='places_list'),
+    path('places/<int:place_id>/', views.place_details_endpoint, name='place_details')
 ]
--- whereToGo/places/views.py
+++ whereToGo/places/views.py
@@ -1,4 +1,40 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from placesService.models import Place
+
 
 def show_map(request):
     return render(request, 'index.html')
+
+def all_places_geojson(request):
+    places = Place.objects.all()
+
+    geojson_features = []
+
+    for place in places:
+        geojson_features.append({
+            'type': 'Feature',
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f'/places/{place.id}'
+            }
+        })
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": geojson_features
+    }
+
+    return JsonResponse(geojson, safe=False)
+
+
+def place_details_endpoint(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+
+    return JsonResponse({"title": place.title}, safe=False)
